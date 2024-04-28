63fba79c83599cef64881672fa846f9c77823b47
level237 <Bramslevel129@gmail.com>
2024-04-29 00:46:51 +0100

    update migration

---
+++ database/migrations/2024_04_28_224939_create_candidates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('age');
+            $table->string('cover_url')->nullable();
+            $table->string('size');
+            $table->integer('score')->default(0);
+            $table->string('profile');
+            $table->string('candidate_slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
