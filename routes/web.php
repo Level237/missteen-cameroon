1cab27193ffc4199147789af6759e402552d1628
level237 <Bramslevel129@gmail.com>
2024-04-29 12:09:31 +0100

    update login

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('test',function(){
+    return view('test');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
