79fb1fcdf1ee6a6a00da451fc5f1b90fa0598f52
level237 <Bramslevel129@gmail.com>
2024-04-29 10:59:21 +0100

    seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'admin',
+            'email'=>'contact@missteencameroon.org',
+            'password'=>Hash::make('password'),
+            'role_id'=>1
+                    ]);
+    }
+}
