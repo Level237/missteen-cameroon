6a77ae24633b05379b1ffc8ea4b7538e6703fcda
level237 <Bramslevel129@gmail.com>
2024-05-01 23:52:00 +0100

    get payToken and accessToken in action

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\Om\GetAccessTokenService;
+use App\Services\Om\InitPaymentService;
 
 class VoteController extends Controller
 {
     public function payment(){
-        $service=(new GetAccessTokenService())->getAccessToken();
-
-        return $service;
+        $accessToken=(new GetAccessTokenService())->getAccessToken();
+        $payToken=(new InitPaymentService())->index($accessToken->access_token);
+        return response()->json(['token'=>$accessToken->access_token,'payToken'=>$payToken],200);
     }
 }
