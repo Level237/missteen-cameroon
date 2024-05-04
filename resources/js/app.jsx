86cb0c4d8bc4756190401dc149fc295e65e3f7ea
level237 <Bramslevel129@gmail.com>
2024-05-04 11:20:28 +0100

    optimizing fetching

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,6 +9,7 @@ export default function App(){
     const link=window.location.href
 
     const [visibleCard,setVisibleCard]=useState(true)
+    const [visibleBtn,setVisibleBtn]=useState(true)
     const [inputError,setInputError]=useState("")
     const [number,setNumber]=useState(null)
     const [error,setError]=useState(null)
@@ -34,7 +35,7 @@ export default function App(){
             catchError(err => {
                 // Gestion des erreurs de réseau ou autres erreurs capturées
                 console.error(err);
-                setError(`une erreur à été  produite,verifié  votre connexion internet et réessayer!`)
+                //setError(`une erreur à été  produite,verifié  votre connexion internet et réessayer!`)
               })
         )
     }
@@ -50,7 +51,7 @@ export default function App(){
             }),
             catchError(err => {
                 // Gestion des erreurs de réseau ou autres erreurs capturées
-                console.error(err);
+                //console.error(err);
 
               })
         )
@@ -74,7 +75,7 @@ export default function App(){
             return;
         }
 
-
+        setVisibleBtn(false)
         const subscription = fetchData(`${href}/access`).subscribe({
             next: result => {
                 console.log(result.token);
@@ -127,26 +128,34 @@ export default function App(){
     }
 
     const getStatus=async()=>{
-        const status=await fetch(`${href}/status/${tokenAccess}/${payToken}/${candidateId}`)
-        const s=await status.json()
-        if(s.status && !s.status.headers ){
-            setStatus(s.status)
-            if(s.status=="PENDING"){
-                setText("Votre paiement est en cours veuillez validez ou tapez #150*50# ne fermez pas la page s'il vous plait!...")
-            }else if(s.status=="CANCELLED"){
+        const subscription3=fetchData(`${href}/status/${tokenAccess}/${payToken}/${candidateId}`).subscribe({
+            next: result => {
+                if(result.status && !result.status.headers ){
+                    setStatus(result.status)
+                    if(result.status=="PENDING"){
+                        setText("Votre paiement est en cours veuillez validez ou tapez #150*50# ne fermez pas la page s'il vous plait!...")
+                    }else if(result.status=="CANCELLED"){
 
-                    setError("Votre paiement a été annuleé!...")
+                            setError("Votre paiement a été annuleé!...")
+                            setInputError('')
+
+
+                    }else if(result.status=='FAILED'){
+                        setError("Votre paiement a été echoué!...")
+                        setInputError('')
+                    }
+                }
 
 
 
-            }else if(s.status=='FAILED'){
-                setError("Votre paiement a été echoué!...")
+            },
+            error: err => {
+                console.error(err);
 
-            }
-        }else{
-            return;
-        }
 
+            },
+            complete: () => console.log('Fetch terminé')
+        })
     }
 
     useEffect(()=>{
@@ -189,7 +198,22 @@ export default function App(){
               <input onChange={numberChanger} value={number} type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  w-full p-4" placeholder="Entrez votre numéro de téléphone" />
             </div>
 
-            <button type='submit'  class="text-white bg-[#0f042d]  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" style={{ paddingLeft:"1.25rem",paddingRight:"1.25rem",paddingTop:"0.625rem",paddingBottom:"0.625rem"  }}>Envoyer</button>
+            {visibleBtn ? <button type='submit'  class="text-white bg-[#0f042d]  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" style={{ paddingLeft:"1.25rem",paddingRight:"1.25rem",paddingTop:"0.625rem",paddingBottom:"0.625rem"  }}>Envoyer</button>: <button type='submit' disabled style={{ background:"#00000079",color:"white",paddingLeft:"1.25rem",paddingRight:"1.25rem",paddingTop:"0.625rem",paddingBottom:"0.625rem" }} class="text-white bg-gray-400  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+
+
+                <div className='flex gap-1 justify-center items-center'>
+
+                <div>
+                En cours d'envoi
+                </div>
+                <div role="status">
+    <svg aria-hidden="true" class="inline w-3 h-3 text-gray-200 animate-spin dark:text-gray-600 fill-red-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+                </div></button>}
           </form>
 
     </div>}
